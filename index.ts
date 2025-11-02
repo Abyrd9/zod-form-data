4945e7f569ce0a634e488402014511d98be2f78e
Andrew Byrd <andrew.byrd99@gmail.com>
2025-11-02 14:56:53 -0500

    Exporting parse result types

--- index.ts
+++ index.ts
@@ -4,6 +4,12 @@ import {
   useZodForm
 } from "./src/index";
 import { parseZodFormData, parseZodData } from "./src/parse-zod-form-data";
+import type { DeepPartial } from "./src/deep-partial";
+import { $ZodType } from "zod/v4/core";
+import z from "zod/v4";
+
+type ZodFormDataParseResultSuccess<T extends $ZodType> = z.infer<T>;
+type ZodFormDataParseResultError<T extends $ZodType> = DeepPartial<NestedFieldErrors<T>> | Record<string, string>;
 
 export {
   useZodForm,
@@ -11,4 +17,6 @@ export {
   parseZodData,
   type FieldProps,
   type NestedFieldErrors as FieldErrors,
+  type ZodFormDataParseResultSuccess,
+  type ZodFormDataParseResultError,
 };
